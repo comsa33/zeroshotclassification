1ecdb8027218055ebc2c0a03accad1a56516422c
ruo <ruo@gravylab.co.kr>
2023-02-28 12:12:06 +0900

    fix: [AI-235]

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-POSTGRE_HOST = "****"
-POSTGRE_PORT = ****
-POSTGRE_USERNAME = "****"
-POSTGRE_PASSWORD = "****"
-POSTGRE_DATABASE = "****"
+POSTGRE_HOST = "59.10.38.6"
+POSTGRE_PORT = 30432
+POSTGRE_USERNAME = "gravylab"
+POSTGRE_PASSWORD = "gravylab2022"
+POSTGRE_DATABASE = "ai_lab"
 
 model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
