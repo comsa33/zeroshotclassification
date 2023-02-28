e00748c2097632dce413940a95adbf5c1cf69c8d
ruo <ruo@gravylab.co.kr>
2023-02-28 11:45:30 +0900

    fix: [AI-235]

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
-MONGO_HOST = "59.10.38.6"
-MONGO_PORT = 30017
-MONGO_USER = "gravylab1"
-MONGO_PSWD = "ahdrh!0720"
+POSTGRE_HOST = "59.10.38.6"
+POSTGRE_PORT = 30432
+POSTGRE_USERNAME = "gravylab"
+POSTGRE_PASSWORD = "gravylab2022"
+POSTGRE_DATABASE = "ai_lab"
+
+model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
